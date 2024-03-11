5129899245697fddcbbe9a6e452fd1e79aad60fe
saki0411 <chipdale411@gmail.com>
2024-03-11 15:34:30 +0900

    add firebase

--- kei-doro/Pages/LoginPage/ViewModels/NFCViewModel.swift
+++ kei-doro/Pages/LoginPage/ViewModels/NFCViewModel.swift
@@ -4,6 +4,7 @@ class NFCViewModel: ObservableObject {
     var savedata: UserDefaults = UserDefaults.standard
     
     func saveUserId(UserId: String){
+        
         savedata.set(UserId, forKey: "userId")
     }
 }
