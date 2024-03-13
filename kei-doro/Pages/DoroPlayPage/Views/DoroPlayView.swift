eaa37b145173a16d313d7ca52403bd8903b2c78a
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-14 00:05:02 +0900

    add:DoroPlayViewの実装

--- kei-doro/Pages/DoroPlayPage/Views/DoroPlayView.swift
+++ kei-doro/Pages/DoroPlayPage/Views/DoroPlayView.swift
@@ -1,146 +1,137 @@
-//
-//  DoroPlayView.swift
-//  kei-doro
-//
-//  Created by 大澤清乃 on 2024/03/13.
-//
 
-import SwiftUI
+ import SwiftUI
+ 
+ struct DoroPlayView: View {
+     var body: some View {
+         ZStack {
+             Color.backColor
+                 .ignoresSafeArea()
+             VStack {
+                 Spacer()
+                 VStack {
+                     OtherTimerLabel()
+                     ZStack(alignment: .center){
+                         GameTimerLabel()
+                         TimerView()
+                     }.padding(.all, 40)
+                 }
+                 Spacer()
+                 HStack {
+                     MissionButton()
+                     Spacer()
+                     CallButton()
+                     Spacer()
+                     MapButton()
+                 }
+                 .frame(width: 320)
+             }
+         }
+     }
+ }
+ 
+ struct OtherTimerLabel: View {
+     var body: some View {
+         HStack {
+             Spacer()
+             VStack {
+                 Text("トランシーバー残量")
+                     .font(.subheadline)
+                     .foregroundStyle(.white)
+                 Text("00:00:00")
+                     .font(.system(size: 32))
+                     .foregroundStyle(.white)
+             }
+             Spacer()
+             //これ必要ないかも
+             VStack {
+                 Text("ミッション終了まで")
+                     .font(.subheadline)
+                     .foregroundStyle(.white)
+                 Text("00:00:00")
+                     .font(.system(size: 32))
+                     .foregroundStyle(.white)
+             }
+             Spacer()
+         }
+     }
+ }
 
-struct DoroPlayView: View {
-    var body: some View {
-        ZStack {
-            Color.backColor
-                .ignoresSafeArea()
-            VStack {
-                Spacer()
-                VStack {
-                    OtherTimerLabel()
-                    ZStack(alignment: .center){
-                        GameTimerLabel()
-                        TimerView()
-                    }.padding(.all, 40)
-                }
-                Spacer()
-                HStack {
-                    MissionButton()
-                    Spacer()
-                    CallButton()
-                    Spacer()
-                    MapButton()
-                }
-                .frame(width: 320)
-            }
-        }
-    }
-}
+ struct GameTimerLabel: View {
+     var body: some View {
+         VStack {
+             Text("ゲーム終了まで")
+                 .font(.subheadline)
+                 .foregroundStyle(.white)
+             Text("00:00:00")
+                 .font(.largeTitle)
+                 .foregroundStyle(.white)
+         }
+     }
+ }
 
-struct OtherTimerLabel: View {
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Text("トランシーバー残量")
-                    .font(.subheadline)
-                    .foregroundStyle(.white)
-                Text("00:00:00")
-                    .font(.system(size: 32))
-                    .foregroundStyle(.white)
-            }
-            Spacer()
-            //これ必要ないかも
-            VStack {
-                Text("ミッション終了まで")
-                    .font(.subheadline)
-                    .foregroundStyle(.white)
-                Text("00:00:00")
-                    .font(.system(size: 32))
-                    .foregroundStyle(.white)
-            }
-            Spacer()
-        }
-    }
-}
+ struct MissionButton: View {
+     var body: some View {
+         Button(action: {
+             print()
+         }, label: {
+             Image(systemName: "checklist")
+                 .font(.system(size: 25))
+                 .foregroundColor(.white)
+                 .frame(width: 80, height: 80)
+                 .background(Color.secondColor)
+                 .cornerRadius(40)
+                 .overlay(
+                     RoundedRectangle(cornerRadius: 50)
+                         .stroke(Color.secondColor, lineWidth: 1))
+         })
+     }
+ }
 
-struct GameTimerLabel: View {
-    var body: some View {
-        VStack {
-            Text("ゲーム終了まで")
-                .font(.subheadline)
-                .foregroundStyle(.white)
-            Text("00:00:00")
-                .font(.largeTitle)
-                .foregroundStyle(.white)
-        }
-    }
-}
-
-struct MissionButton: View {
-    var body: some View {
-        Button(action: {
-            print()
-        }, label: {
-            Image(systemName: "checklist")
-                .font(.system(size: 25))
-                .foregroundColor(.white)
-                .frame(width: 80, height: 80)
-                .background(Color.secondColor)
-                .cornerRadius(40)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 50)
-                        .stroke(Color.secondColor, lineWidth: 1))
-        })
-    }
-}
-
-struct CallButton: View {
-    @State var showNextView: Bool = false
-    var body: some View {
-        NavigationStack {
-            Button(action: {
-                self.showNextView = true
-            }, label: {
-                Image(systemName: "mic.fill")
-                    .font(.system(size: 25))
-                    .foregroundColor(.white)
-                    .frame(width: 120, height: 120)
-                    .background(Color.secondColor)
-                    .cornerRadius(60)
-                //                    .overlay(
-                //                        RoundedRectangle(cornerRadius: 50)
-                //                            .stroke(Color.secondColor, lineWidth: 1))
-            })
-            .sheet(isPresented: $showNextView, content: {
-                CallView()
-            })
-        }
-        .frame(width: 120, height: 120)
-        .cornerRadius(60)
-        .overlay(
-            RoundedRectangle(cornerRadius: 60)
-                .stroke(Color.mainColor, lineWidth: 4))
-    }
-}
-
-struct MapButton: View {
-    var body: some View {
-        Button(action: {
-            print()
-        }, label: {
-            Image(systemName: "map")
-                .font(.system(size: 25))
-                .foregroundColor(.white)
-                .frame(width: 80, height: 80)
-                .background(Color.secondColor)
-                .cornerRadius(40)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 50)
-                        .stroke(Color.secondColor, lineWidth: 1))
-        })
-    }
-}
-
-
-#Preview {
-    DoroPlayView()
-}
+ struct CallButton: View {
+     @State var showNextView: Bool = false
+     var body: some View {
+         NavigationStack {
+             Button(action: {
+                 self.showNextView = true
+             }, label: {
+                 Image(systemName: "mic.fill")
+                     .font(.system(size: 25))
+                     .foregroundColor(.white)
+                     .frame(width: 120, height: 120)
+                     .background(Color.secondColor)
+                     .cornerRadius(60)
+             })
+             .sheet(isPresented: $showNextView, content: {
+                 CallView()
+             })
+         }
+         .frame(width: 120, height: 120)
+         .cornerRadius(60)
+         .overlay(
+             RoundedRectangle(cornerRadius: 60)
+                 .stroke(Color.mainColor, lineWidth: 4))
+     }
+ }
+ 
+ struct MapButton: View {
+     var body: some View {
+         Button(action: {
+             print()
+         }, label: {
+             Image(systemName: "map")
+                 .font(.system(size: 25))
+                 .foregroundColor(.white)
+                 .frame(width: 80, height: 80)
+                 .background(Color.secondColor)
+                 .cornerRadius(40)
+                 .overlay(
+                     RoundedRectangle(cornerRadius: 50)
+                         .stroke(Color.secondColor, lineWidth: 1))
+         })
+     }
+ }
+ 
+ 
+ #Preview {
+     DoroPlayView()
+ }
--- kei-doro/Pages/TimerPage/ViewModels/CircleProgressViewModel.swift
+++ kei-doro/Pages/TimerPage/ViewModels/CircleProgressViewModel.swift
@@ -1,34 +1,28 @@
-//
-//  CircleProgressViewModel.swift
-//  kei-doro
-//
-//  Created by 大澤清乃 on 2024/03/13.
-//
 
-import Foundation
-import SwiftUI
-import Combine
+ import Foundation
+ import SwiftUI
+ import Combine
+ 
+ final class CircleProgressViewModel: ObservableObject {
+     @Published var progressValue: CGFloat = 0.0
+     private var timerCount: CGFloat = 0.0
+     private var cancellable: AnyCancellable?
+ 
+     init() {
+         startTimer()
+     }
+ 
+     private func startTimer() {
+         cancellable = Timer.publish(every: 0.1, on: .main, in: .default)
+             .autoconnect()
+             .sink { [weak self] _ in
+                 self?.countProgress()
+             }
+     }
 
-final class CircleProgressViewModel: ObservableObject {
-    @Published var progressValue: CGFloat = 0.0
-    private var timerCount: CGFloat = 0.0
-    private var cancellable: AnyCancellable?
-    
-    init() {
-        startTimer()
-    }
-    
-    private func startTimer() {
-        cancellable = Timer.publish(every: 0.1, on: .main, in: .default)
-            .autoconnect()
-            .sink { [weak self] _ in
-                self?.countProgress()
-            }
-    }
-    
-    private func countProgress() {
-        if timerCount > 60 { cancellable?.cancel() }
-        timerCount = timerCount + 0.1
-        progressValue = timerCount / 60.0
-    }
-}
+     private func countProgress() {
+         if timerCount > 60 { cancellable?.cancel() }
+         timerCount = timerCount + 0.1
+         progressValue = timerCount / 60.0
+     }
+ }
