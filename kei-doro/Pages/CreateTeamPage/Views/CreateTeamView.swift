7b889ed584f586da8ad755da7b2899bedeb4c8d6
saki0411 <chipdale411@gmail.com>
2024-03-13 12:08:37 +0900

    addcreateTeamView

--- kei-doro/Pages/CreateTeamPage/Views/CreateTeamView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CreateTeamView.swift
-//  kei-doro
-//
-//  Created by saki on 2024/03/13.
-//
-
-import Foundation
--- kei-doro/Pages/HomePage/Views/HomeView.swift
+++ kei-doro/Pages/HomePage/Views/HomeView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct HomeView: View {
+
+    @State var createTeam = false
     var body: some View {
+        
         VStack {
-            NavigationView {
-                NavigationLink(destination: NFCView()) {
-                    Text("START").bold().padding().frame(width: 200, height: 100).foregroundColor(Color.black).background(Color.mainColor).cornerRadius(10)
+            VStack{
+                NFCView()
+                Button(action: {
+                    createTeam.toggle()
+                }, label: {
+                    Text("チーム作成")
+                })
+                .padding(.top,400)
+                .sheet(isPresented: $createTeam) {
+                    CreateTeamView( gameMasterId: "", userId: "", gameId: "", picktime: "10")
                 }
+              
+                Button(action: {
+                    
+                }, label: {
+                    Text("チーム参加")
+                })
+              
+            }
+         
+                  .bold()
+                  .padding()
+                  .frame(width: 200, height: 100)
+                  .foregroundColor(Color.black)
+                  .background(Color.mainColor)
+                  .cornerRadius(10)
+                
 //                NavigationLink(destionation: ) {
 //                                    Text("GAMEに参加する").bold().padding().frame(width: 200, height: 100).foregroundColor(Color.black).background(Color.mainColor).cornerRadius(10)
 //                }
-            }
+            
         }
     }
 }
