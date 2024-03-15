487cc2c88d0ca19cdaba330ac93defaf3f71a2d0
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-15 20:43:41 +0900

    fix: NFCVIewの編集

--- kei-doro/Pages/LoginPage/Views/NFCView.swift
+++ kei-doro/Pages/LoginPage/Views/NFCView.swift
@@ -8,6 +8,53 @@ struct NFCView: View {
     
     var randomId = UUID().uuidString
     var body: some View {
+        ZStack {
+            Color.backColor
+                .ignoresSafeArea()
+            VStack {
+                Image("logoImage")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                    .scaledToFit()
+                VStack {
+                    getNFC()
+                    getTestNFC()
+                        .padding(20)
+                }
+            }
+        }
+        .alert(isPresented: $isAlertShown) {
+            Alert(
+                title: Text(""),
+                message: Text(alertMessage),
+                dismissButton: .default(Text("OK")))
+        }
+        //シュミレーターでやる人はこの辺のコード入ります
+        
+//        .onAppear(){
+//            var savedata: UserDefaults = UserDefaults.standard
+//            let random = UUID().uuidString
+//            savedata.set(random, forKey: "UserId")
+//            
+//            Task{
+//                do{
+//                    try await viewModel.saveUserId(UserId: random)
+//                }
+//                catch{
+//                    print(error)
+//                }
+//            }
+//        }
+    }
+}
+
+struct getNFC: View {
+    @StateObject  var session = NFCSession()
+    @ObservedObject var viewModel = NFCViewModel()
+    @State private var alertMessage = ""
+    @State private var isAlertShown = false
+    
+    var body: some View {
         Button(action: {
             session.startReadSession { text, error in
                 if let error = error {
@@ -32,9 +79,31 @@ struct NFCView: View {
             
         }, label: {
             Text("NFC読み込み")
+                .foregroundStyle(Color.secondColor)
+                .font(.system(size: 22))
+                .bold()
+//                .underline()
+                .frame(width: 320, height: 56)
+//                .background(Color.secondColor)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(Color.secondColor, lineWidth: 5)
+                )
         })
+        .cornerRadius(4)
+    }
+}
+
+struct getTestNFC: View {    
+    @StateObject  var session = NFCSession()
+    @ObservedObject var viewModel = NFCViewModel()
+    @State private var alertMessage = ""
+    @State private var isAlertShown = false
+    
+    var randomId = UUID().uuidString
+
+    var body: some View {
         Button(action: {
-            
             session.startWriteSession(text: randomId) { error in
                 if let error = error {
                     alertMessage = error.localizedDescription
@@ -44,31 +113,9 @@ struct NFCView: View {
             
         }, label: {
             Text("テスト用NFC書き込み")
+                .foregroundStyle(Color.secondColor)
         })
-        .alert(isPresented: $isAlertShown) {
-            Alert(
-                title: Text(""),
-                message: Text(alertMessage),
-                dismissButton: .default(Text("OK")))
-        }
-        //シュミレーターでやる人はこの辺のコード入ります
-        
-//        .onAppear(){
-//            var savedata: UserDefaults = UserDefaults.standard
-//            let random = UUID().uuidString
-//            savedata.set(random, forKey: "UserId")
-//            
-//            Task{
-//                do{
-//                    try await viewModel.saveUserId(UserId: random)
-//                }
-//                catch{
-//                    print(error)
-//                }
-//            }
-//        }
     }
-    
 }
 
 #Preview {
