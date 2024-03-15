49444831971ed76e6b26719b3098cd93992d46a0
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-16 00:18:38 +0900

    add: HomeView

--- kei-doro/Pages/HomePage/Views/HomeView.swift
+++ kei-doro/Pages/HomePage/Views/HomeView.swift
@@ -5,100 +5,100 @@
 //  Created by 布川のぞみ on 2024/03/10.
 //
 
-import SwiftUI
+ import SwiftUI
 
-struct HomeView: View {
-    @ObservedObject var viewModel = HomeViewModel()
-    @State var createTeam = false
-    @State var JoinTeam = false
-    @State var boolGameTeam = true
-    @State var errorArart = false
-    @State var gameId = ""
-    @State var openJoinView = false
-    @State var name = ""
-    var body: some View {
+ struct HomeView: View {
+     @ObservedObject var viewModel = HomeViewModel()
+     @State var createTeam = false
+     @State var JoinTeam = false
+     @State var boolGameTeam = true
+     @State var errorArart = false
+     @State var gameId = ""
+     @State var openJoinView = false
+     @State var name = ""
+     var body: some View {
         
-        ZStack {
-            Color.backColor
-                .ignoresSafeArea()
-            VStack {
-                Spacer()
-                    .overlay(
-                        VStack{
-                            userButton()
-                                .padding(.top, 16)
-                            Spacer()
-                        }
-                    )
-                VStack {
-                    Button(action: {
-                        createTeam.toggle()
-                    }, label: {
-                        Text("チーム作成")
-                    })
-                    .font(.system(size: 22))
-                    //            .bold()
-                    .foregroundColor(.white)
-                    .frame(width: 320, height: 72)
-                    .background(Color.secondColor)
-                    .cornerRadius(5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.mainColor, lineWidth: 2))
-                    .padding(.bottom, 40)
-                    
-                    Button(action: {
-                        JoinTeam.toggle()
-                    }, label: {
-                        Text("IDを入力")
-                    })
-                    .font(.system(size: 22))
-                    .foregroundColor(.white)
-                    .frame(width: 320, height: 72)
-                    .background(Color.secondColor)
-                    .cornerRadius(5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.mainColor, lineWidth: 2)
-                    )
-                    .sheet(isPresented: $createTeam) {
-                        CreateTeamView(userId: "", gameId: "", picktime: "10", pickOni:  "1", gameMasterName: "")
-                    }
-                }
-                Spacer()
-            }
-            .alert("ゲームIDを入力", isPresented: $JoinTeam) {
-                TextField("ゲームID", text: $gameId)
-                Button {
-                    Task {
-                        do{
-                            name = try await viewModel.makeIcon()
-                            boolGameTeam =  try await viewModel.searchgame(gameId: gameId, name: name)
-                            if boolGameTeam == false{
-                                errorArart.toggle()
-                            }else{
-                                openJoinView.toggle()
-                            }
-                            
-                        }
-                    }
-                    
-                } label: {
-                    Text("OK")
-                }
-            }
+         ZStack {
+             Color.backColor
+                 .ignoresSafeArea()
+             VStack {
+                 Spacer()
+                     .overlay(
+                         VStack{
+                             userButton()
+                                 .padding(.top, 16)
+                             Spacer()
+                         }
+                     )
+                 VStack {
+                     Button(action: {
+                         createTeam.toggle()
+                     }, label: {
+                         Text("チーム作成")
+                     })
+                     .font(.system(size: 22))
+                     //            .bold()
+                     .foregroundColor(.white)
+                     .frame(width: 320, height: 72)
+                     .background(Color.secondColor)
+                     .cornerRadius(5)
+                     .overlay(
+                         RoundedRectangle(cornerRadius: 5)
+                             .stroke(Color.mainColor, lineWidth: 2))
+                     .padding(.bottom, 40)
             
-            .alert("ゲームIDが間違っています", isPresented: $errorArart) {
-                Button {
-                } label: {
-                    Text("OK")
-                }
-            }
-            .sheet(isPresented: $openJoinView){
-                JoinTeamView(userId: "", gameId: gameId, time: "", oni: "")
-                
-            }
-        }
+                     Button(action: {
+                         JoinTeam.toggle()
+                     }, label: {
+                         Text("IDを入力")
+                     })
+                     .font(.system(size: 22))
+                     .foregroundColor(.white)
+                     .frame(width: 320, height: 72)
+                     .background(Color.secondColor)
+                     .cornerRadius(5)
+                     .overlay(
+                         RoundedRectangle(cornerRadius: 5)
+                             .stroke(Color.mainColor, lineWidth: 2)
+                     )
+                     .sheet(isPresented: $createTeam) {
+                         CreateTeamView(userId: "", gameId: "", picktime: "10", pickOni:  "1", gameMasterName: "")
+                     }
+                 }
+                 Spacer()
+             }
+             .alert("ゲームIDを入力", isPresented: $JoinTeam) {
+                 TextField("ゲームID", text: $gameId)
+                 Button {
+                     Task {
+                         do{
+                             name = try await  viewModel.makeIcon()
+                             boolGameTeam =  try await viewModel.searchgame(gameId: gameId, name: name)
+                             if boolGameTeam == false{
+                                 errorArart.toggle()
+                             }else{
+                                 openJoinView.toggle()
+                             }
+         
+                         }
+                     }
+            
+                 } label: {
+                     Text("OK")
+                 }
+             }
+      
+             .alert("ゲームIDが間違っています", isPresented:  $errorArart) {
+                 Button {
+                 } label: {
+                     Text("OK")
+                 }
+             }
+             .sheet(isPresented: $openJoinView){
+                 JoinTeamView(userId: "", gameId: gameId, time: "", oni: "")
+         
+             }
+         }
         
         
         
@@ -107,21 +107,21 @@ struct HomeView: View {
         //        Text("GAMEに参加する").bold().padding().frame(width: 200, height: 100).foregroundColor(Color.black).background(Color.mainColor).cornerRadius(10)
         //
         
-    }
-}
+     }
+ }
 
-struct userButton: View {
-    var body: some View {
-        Button(action: {
-            print()
-        }, label: {
-            Image(systemName: "person.circle")
-                .font(.system(size: 40))
-                .foregroundColor(Color.secondColor)
-                .frame(width: 320, height: 72, alignment: .trailing)
-        })
-    }
-}
+ struct userButton: View {
+     var body: some View {
+         Button(action: {
+             print()
+         }, label: {
+             Image(systemName: "person.circle")
+                 .font(.system(size: 40))
+                 .foregroundColor(Color.secondColor)
+                 .frame(width: 320, height: 72, alignment:  .trailing)
+         })
+     }
+ }
 
 #Preview {
     HomeView()
