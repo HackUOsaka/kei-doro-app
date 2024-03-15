f393b9e5eaf5464e5015e8219cf72c78fdf62286
saki0411 <chipdale411@gmail.com>
2024-03-16 06:43:53 +0900

    add-nfc-userId

--- CreateUserView.swift
+++ CreateUserView.swift
@@ -34,6 +34,7 @@ struct CreateUserView: View {
     @ObservedObject var viewModel = CreateUserViewModel()
     @State var name = ""
     @State private var isHomeViewActive = false // 追加：HomeViewの表示状態を管理するState
+    @StateObject var session = NFCSession()
 
     var body: some View {
         NavigationView { // 追加：NavigationViewでラップする
@@ -58,7 +59,14 @@ struct CreateUserView: View {
                     Text("ユーザーを登録する")
                 }
                 Button(action: {
-                    
+              
+
+                    session.startWriteSession(text: viewModel.userId) { error in
+                                                   if let error = error {
+                                                      print(error)
+                                                   }
+                                               }
+
                 }, label: {
                     Text("NFC書き込み")
                 })
