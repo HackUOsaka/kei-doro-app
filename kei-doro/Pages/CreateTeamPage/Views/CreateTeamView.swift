f3fead1fb4746bdd823fd3060ea49f4c8559de4b
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-18 02:53:57 +0900

    add: startする前にアラート出てくるように！

--- kei-doro/Pages/CreateTeamPage/Views/CreateTeamView.swift
+++ kei-doro/Pages/CreateTeamPage/Views/CreateTeamView.swift
@@ -12,14 +12,7 @@ struct CreateTeamView: View {
 //    @EnvironmentObject var timeManager: TimeManager
     
     @ObservedObject var viewModel = CreateTeamViewModel()
-//    @State var userId: String
-//    @State var gameId: String
-//    @State var picktime: String
-//    @State var pickOni: String
-//    @State var gameMasterName: String
-//    @State var gameStart = false
-//    @State var users = [String]()
-//    @State var names = [String]()
+    @State private var startShow: Bool = false
     @State var showPlayView: Bool = false
     
     let savedata: UserDefaults = UserDefaults.standard
@@ -33,98 +26,56 @@ struct CreateTeamView: View {
             Color.backColor
                 .ignoresSafeArea()
             VStack {
-                HStack{
+                VStack{
                     Text("GameID：")
                     Text(gameId)
+                        .font(.system(size: 32))
+                        .padding(.top, 8)
                 }
                 .foregroundColor(.white)
                 .padding()
-                Button(action: {
-                    self.showPlayView = true
-                    Task {
-                        do{
-                            try await viewModel.postGameData(gameId: gameId, userId: userId!)
-                        } catch {
-                            print ()
-                        }
-                    }
+                Button(action: { startButtonAction()
                 }, label: {
                     Text("Start")
+                        .font(.system(size: 20))
+                        .foregroundStyle(.white)
+                        .frame(width: 320, height: 64)
+                        .background(Color.secondColor)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(Color.mainColor, lineWidth: 2)
+                        )
                 })
+                .alert("ゲームを始めますか？", isPresented: $startShow) {
+                    Button("Cancel", role: .cancel) {}
+                    Button("Ok") {
+                        self.showPlayView = true
+                    }
+                }
                 .fullScreenCover(isPresented: $showPlayView, content: {
                     PlayView()
                 })
             }
         }
     }
+    
+    private func startButtonAction() {
+//        self.showPlayView = true
+        startShow.toggle()
+        let userId = savedata.string(forKey: "userId")
+        let gameId = savedata.string(forKey: "gameId")!
+        Task {
+            do{
+                try await viewModel.postGameData(gameId: gameId, userId: userId!)
+            } catch {
+                print ()
+            }
+        }
+    }
 }
-    //        .onAppear(){
-    //            Task{
-    //                do{
-    //                    gameMasterName = try await viewModel.makeIcon()
-    //                    userId = try await viewModel.getUserId()
-    //                    gameId = try await viewModel.createGameId(userId: userId, name: gameMasterName)
-    //                    try await  getUsers(gameId: gameId)
-    //                }
-    //            }
-    //        }
-    //
-    //        .onChange(of: users){
-    //            print("a")
-    //        }
-    //
-    //        .onChange(of: picktime){
-    //            Task{
-    //                do{
-    //                    try await viewModel.saveTime(gameId: gameId, limit: "3")
-    //                }
-    //            }
-    //        }
-    //        .onChange(of: pickOni){
-    //            Task{
-    //                do{
-    //                    try await viewModel.saveOni(gameId: gameId, Oni: pickOni)
-    //                }
-    //            }
-    //        }
-    //        //ゲームスタートの処理
-    //        .onChange(of: gameStart){
-    //            Task{
-    //                do{
-    //                    try await viewModel.startTimer(gameId: gameId)
-    //                }
-    //            }
-    //        }
-    //
-    //    }
-    //
-    //    func getUsers(gameId: String)async throws{
-    //        var userName = [String]()
-    //        let db = Firestore.firestore()
-    //
-    //        db.collection("games").document(gameId)
-    //            .addSnapshotListener { documentSnapshot, error in
-    //                guard let document = documentSnapshot else {
-    //                    print("Error fetching document: \(error!)")
-    //                    return
-    //                }
-    //                guard let data = document.data() else {
-    //                    print("Document data was empty.")
-    //                    return
-    //                }
-    //                let playerIds = data["names"] as![String]
-    //                userName = []
-    //                users = []
-    //                for name in playerIds{
-    //                    let index = name.startIndex
-    //                    let firstName = String(name[index])
-    //                    print(name)
-    //                    users.append(firstName)
-    //                }
-    //                print("Current data: \(userName)")
-    //
-    //            }
-    //
-    //    }
-    //}
+
+
+#Preview {
+    CreateTeamView()
+}
 
